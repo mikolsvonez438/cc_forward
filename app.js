969c93375a9466f5e987a3186b615fbf47f751ab
nell4438 <98459805+nell4438@users.noreply.github.com>
2024-09-04 09:53:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -30,14 +30,14 @@ app.get('/checkcc', async (req, res) => {
 
 app.get('/shortme', async (req, res) => {
     const { url } = req.query;
-  const urlRegex = /^(?:http(s)?:\/\/)?[a-zA-Z0-9][-a-zA-Z0-9]*\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?(?:\/?.*)$/;
+    const urlRegex = /^(?:http(s)?:\/\/)?[a-zA-Z0-9][-a-zA-Z0-9]*\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?(?:\/?.*)$/;
 
-  if (!urlRegex.test(url)) {
-    return res.status(400).send('Invalid URL');
-  }
+    if (!urlRegex.test(url)) {
+        return res.status(400).send('Invalid URL');
+    }
     try {
         const response = await axios.get(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
-        res.json(response.data);
+        res.json({ shortLink: response.data });
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).send('Server error');
@@ -46,7 +46,7 @@ app.get('/shortme', async (req, res) => {
 
 // app.get('/verifyme', async (req, res) => {
 //     const { email } = req.query;
-     
+
 //     try {
 //         const response = await axios.get(`https://verifymail.io/api/${email}?key=388353020d35419692677341e3046106`);
 //         res.json(response.data);

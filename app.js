1674d08e9b03ec6565d220dad90815d0f87684bc
nell4438 <98459805+nell4438@users.noreply.github.com>
2024-09-07 12:59:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,85 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+const { loadStripe } = require("@sergdudko/stripe-js");
+app.use(cors());
+
 app.get('/teststripe', async (req, res) => {
     const stripe = await loadStripe("pi_3PwGU9F6KbQh44q10EuDUs92_secret_gNSviAhCchmkSgVLmtWInwWOV", "pm_1PwGU7F6KbQh44q1QVkZaVMy", "https://suschegg.com/dashboard?msg=Payment Successful&status=success");
     res.send(stripe);
 })
+app.get('/check', async (req, res) => {
+    const { cc } = req.query;
+    try {
+        const response = await axios.get(`https://xchecker.cc/api.php?cc=${cc}`);
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Server error');
+    }
+});
+
+app.get('/checkcc', async (req, res) => {
+    const { binValue } = req.query;
+    try {
+        const response = await axios.get(`https://binlist.io/lookup/${binValue}`);
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Server error');
+    }
+});
+
+app.get('/checkccs', async (req, res) => {
+    const { bins } = req.query;
+    try {
+        if (!Array.isArray(bins)) {
+            return res.status(400).send('Invalid input. Please provide an array of bins.');
+        }
+
+        const requests = bins.map(binValue => axios.get(`https://binlist.io/lookup/${binValue}`));
+        const responses = await Promise.all(requests);
+        const results = responses.map(response => response.data);
+
+        res.json(results);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Server error');
+    }
+});
+
+
+
+app.get('/shortme', async (req, res) => {
+    const { url } = req.query;
+    const urlRegex = /^(?:http(s)?:\/\/)?[a-zA-Z0-9][-a-zA-Z0-9]*\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?(?:\/?.*)$/;
+
+    if (!urlRegex.test(url)) {
+        return res.status(400).send('Invalid URL');
+    }
+    try {
+        const response = await axios.get(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
+        res.json({ shortLink: response.data });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Server error');
+    }
+});
+
+// app.get('/verifyme', async (req, res) => {
+//     const { email } = req.query;
+
+//     try {
+//         const response = await axios.get(`https://verifymail.io/api/${email}?key=388353020d35419692677341e3046106`);
+//         res.json(response.data);
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//         res.status(500).send('Server error');
+//     }
+// });
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Proxy server running on port ${PORT}`);
+});
